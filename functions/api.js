89d7df3ea69df8ae7b0a19d818ea8f3302ba8dbd
Arslan_Azhar <arslankhandemo@gmail.com>
2023-05-16 19:33:18 +0500

    adding functionality of setting enviroment veriables

--- functions/api.js
+++ functions/api.js
@@ -1,20 +1,21 @@
+require("../ENV_SETTING/env_setting")();
 const express = require("express");
 const serverless = require("serverless-http"); 
 const router = require("express").Router();
 //const ejs = require("");
 const app = express();
-require("dotenv").config();
+
 
 app.set("view engine","ejs");
 
-
 router.get("/",(req,res)=>{
     //res.render("../view/index");
+    console.log("ENVIROMENT: ",process.env.ENVIROMENT);
     res.json({"ALERT":"App page is workiiiiing"});
 });
 
 let PORT = process.env.PORT || 5000;
-console.log(__dirname);
+//console.log(__dirname);
 app.listen(3000,()=>{
     console.log(`The app is running on port:${PORT}`);
 });
